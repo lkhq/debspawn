2fc18602f6816c0b1831094850648d3c05337a8b
Matthias Klumpp <matthias@tenstral.net>
2021-11-06 23:39:16 +0100

    Add helper script to quickly lint everything

---
+++ lint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+
+echo "=== Flake8 ==="
+python -m flake8 ./ --statistics
+python -m flake8 debspawn/dsrun --statistics
+echo "✓"
+
+echo "=== Pylint ==="
+python -m pylint -f colorized ./debspawn
+python -m pylint -f colorized ./debspawn/dsrun
+python -m pylint -f colorized ./tests ./data
+python -m pylint -f colorized setup.py install-sysdata.py
+echo "✓"
+
+echo "=== MyPy ==="
+python -m mypy .
+python -m mypy ./debspawn/dsrun
+echo "✓"
